bf02b20072303ecf1477391cb6fc70a9bc0e796e
gregory1506 <gjotomcat@gmail.com>
2017-11-11 02:39:11 -0400

    dfgjsrg

--- Assignment3/workserver.py
+++ Assignment3/workserver.py
@@ -5,10 +5,16 @@ import math
 import socket
 import threading
 import time
-from bottle import route, run
-from azure.storage.queue import QueueService
-queue_service = QueueService(account_name='gjocloudassignment3',\
+import json
+from bottle import route, run, request
+#from azure.storage.queue import QueueService
+from azure.storage.table import TableService, Entity
+#queue_service = QueueService(account_name='gjocloudassignment3',\
+#    account_key='7+WP5BOHFDDsBON6qqdqD8YdzQxUuP2jlcOIR/0b0Qvh/gduEVDI0YnoLYN82tZFq58H82+TdtDdsRJfOP+hjA==')
+table_service = TableService(account_name='gjocloudassignment3',\
     account_key='7+WP5BOHFDDsBON6qqdqD8YdzQxUuP2jlcOIR/0b0Qvh/gduEVDI0YnoLYN82tZFq58H82+TdtDdsRJfOP+hjA==')
+if not table_service.exists('Transactions'):
+    table_service.create_table('Transactions')
 
 hostname = socket.gethostname()
 hostport = 9000
@@ -31,39 +37,46 @@ worker_thread = threading.Thread(target=workerthread, args=())
 worker_thread.start()
 
 
-def writebody():
-    body = '<html><head><title>Work interface - build</title></head>'
-    body += '<body><h2>Worker interface on ' + hostname + '</h2><ul><h3>'
+#def writebody():
+#    body = '<html><head><title>work interface - build</title></head>'
+#    body += '<body><h2>worker interface on ' + hostname + '</h2><ul><h3>'
 
-    if keepworking == False:
-        body += '<br/>Worker thread is not running. <a href="./do_work">Start work</a><br/>'
-    else:
-        body += '<br/>Worker thread is running. <a href="./stop_work">Stop work</a><br/>'
+#    if keepworking == false:
+#        body += '<br/>worker thread is not running. <a href="./do_work">start work</a><br/>'
+#    else:
+#        body += '<br/>worker thread is running. <a href="./stop_work">stop work</a><br/>'
 
-    body += '<br/>Usage:<br/><br/>/do_work = start worker thread<br/>/stop_work = stop worker thread<br/>'
-    body += '</h3></ul></body></html>'
-    return body
+#    body += '<br/>usage:<br/><br/>/do_work = start worker thread<br/>/stop_work = stop worker thread<br/>'
+#    body += '</h3></ul></body></html>'
+#    return body
 
 
-@route('/')
+@route('/',method='POST')
 def root():
-    return writebody()
+    data = request.json
+    new_entity = Entity()
+    new_entity.PartitionKey = data['TransactionID']
+    new_entity.RowKey = data['UserId']
+    new_entity.Sellerid = data['Sellerid']
+    new_entity.ProductName = entity['ProductName']
+    new_entity.SalePrice = Entity['SalePrice']
+    new_entity.TransactionDate = Entity['TransactionDate']
+    mystorage.table_service.insert_or_replace_entity('Shots',new_entity)
+
+#@route('/do_work')
+#def do_work():
+#    global keepworking
+#    # start worker thread
+#    keepworking = True
+#    return writebody()
 
 
-@route('/do_work')
-def do_work():
-    global keepworking
-    # start worker thread
-    keepworking = True
-    return writebody()
-
-
-@route('/stop_work')
-def stop_work():
-    global keepworking
-    # stop worker thread
-    keepworking = False
-    return writebody()
+#@route('/stop_work')
+#def stop_work():
+#    global keepworking
+#    # stop worker thread
+#    keepworking = False
+#    return writebody()
 
 
 run(host=hostname, port=hostport)
