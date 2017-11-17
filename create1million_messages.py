bad95d10354c117b34f015a91cd157a5d9c60a75
gregory1506 <gjotomcat@gmail.com>
2017-11-17 14:41:16 -0400

    fcdsxgbf

--- create1million_messages.py
+++ create1million_messages.py
@@ -7,8 +7,6 @@ from sastoken import get_auth_token
 
 sas = get_auth_token("gregseon4e059a98c11c","taskqueue","RootManageSharedAccessKey","d8PrqA7to95t0wUFywAfhNDcbUwvh2sIpiHqvUdbPSQ=")
 
-
-
 async def sendmsg(data,session):
     ''' send message async '''
     global sas
@@ -16,8 +14,8 @@ async def sendmsg(data,session):
     URL = "https://gregseon4e059a98c11c.servicebus.windows.net/taskqueue/messages"
     async with session.post(URL, data=data, headers=headers) as response:
         if response.status != 201:
-            sendmsg(data,session)
-            print(str(time.time()) + " " + data + "\n")
+            asyncio.sleep(5) # sort of like a circuit breaker pattern. Wait 5 seconds and retry
+            await sendmsg(data, session)
         return await response.read() 
 
 async def boundsendmsg(sem, data, session):
@@ -34,14 +32,16 @@ async def run(r):
         for _ in range(r):
             userid = "A" + str(random.randint(1,1000))
             sellerid = "S" + str(random.randint(1,1000))
+            failure = "no"
+            if _ % 50000 == 0:
+                failure = "yes"
             data = {"TransactionID":int(time.time()),"UserId":userid,'SellerID':sellerid,'ProductName':random.choice(products),"SalePrice":random.randint(1000,1000000),\
-                    'TransactionDate':time.strftime("%Y-%m-%d")}
+                    'TransactionDate':time.strftime("%Y-%m-%d"),'failure':failure}
             task = asyncio.ensure_future(boundsendmsg(sem,json.dumps(data), session))
             tasks.append(task)
             if _ % 1000 == 0:
                 print(_)
         await asyncio.gather(*tasks)
-
 N = 1000000
 LOOP = asyncio.get_event_loop()
 FUTURE = asyncio.ensure_future(run(N))
--- servicebusasync_recieve.py
+++ servicebusasync_recieve.py
@@ -5,21 +5,20 @@ import asyncio
 from aiohttp import ClientSession
 from sastoken import get_auth_token
 
-sas = get_auth_token("gregseon4e059a98c11c","test4scaling","RootManageSharedAccessKey","d8PrqA7to95t0wUFywAfhNDcbUwvh2sIpiHqvUdbPSQ=")
-
+sas = get_auth_token("gregseon4e059a98c11c","test5scaling","RootManageSharedAccessKey","d8PrqA7to95t0wUFywAfhNDcbUwvh2sIpiHqvUdbPSQ=")
 
 
 async def getmsg(session):
     ''' send message async '''
     global sas
     headers = {'Authorization':sas["token"],'Content-Type': 'application/atom+xml;type=entry;charset=utf-8'}
-    URL = "https://gregseon4e059a98c11c.servicebus.windows.net/test4scaling/messages/head"
+    URL = "https://gregseon4e059a98c11c.servicebus.windows.net/test5scaling/messages/head"
     async with session.post(URL, headers=headers) as response:
-        assert response.status == 201
-        print([x async for x in response.content][0])
-        msg = json.loads(response.headers.get("BrokerProperties"))
-        print(msg["LockToken"])
-        print(msg["MessageId"])
+        if response.status != 201:
+            await getmsg(session) # add another read if https breaks downs for this read. Message should still be in queue and unlocked for another competing consumer.
+        msg = json.loads([x async for x in response.content][0])
+        msginfo = json.loads(response.headers.get("BrokerProperties"))
+        print("{} {} {}".format(msginfo["LockToken"],msginfo["MessageId"],msg["failure"]))
         return await response.read() 
 
 async def boundgetmsg(sem, session):
@@ -38,7 +37,7 @@ async def run(r):
         responses = asyncio.gather(*tasks)
         await responses       
 
-N = 20
+N = 1000000
 LOOP = asyncio.get_event_loop()
 FUTURE = asyncio.ensure_future(run(N))
 LOOP.run_until_complete(FUTURE)
