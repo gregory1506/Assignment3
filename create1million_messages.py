6e0a083de6495fc607dbf69039a785bbc043b80e
gregory1506 <gjotomcat@gmail.com>
2017-11-18 11:23:26 -0400

    sfgsdhd

--- create1million_messages.py
+++ create1million_messages.py
@@ -5,13 +5,13 @@ import asyncio
 from aiohttp import ClientSession
 from sastoken import get_auth_token
 
-sas = get_auth_token("gregseon4e059a98c11c","taskqueue","RootManageSharedAccessKey","d8PrqA7to95t0wUFywAfhNDcbUwvh2sIpiHqvUdbPSQ=")
+sas = get_auth_token("gregseon4e059a98c11c","finalqueue","RootManageSharedAccessKey","d8PrqA7to95t0wUFywAfhNDcbUwvh2sIpiHqvUdbPSQ=")
 
 async def sendmsg(data,session):
     ''' send message async '''
     global sas
     headers = {'Authorization':sas["token"],'Content-Type':'Content-Type: application/vnd.microsoft.servicebus.json'}
-    URL = "https://gregseon4e059a98c11c.servicebus.windows.net/taskqueue/messages"
+    URL = "https://gregseon4e059a98c11c.servicebus.windows.net/finalqueue/messages"
     async with session.post(URL, data=data, headers=headers) as response:
         if response.status != 201:
             asyncio.sleep(5) # sort of like a circuit breaker pattern. Wait 5 seconds and retry
@@ -42,7 +42,7 @@ async def run(r):
             if _ % 1000 == 0:
                 print(_)
         await asyncio.gather(*tasks)
-N = 1000000
+N = 300000
 LOOP = asyncio.get_event_loop()
 FUTURE = asyncio.ensure_future(run(N))
 LOOP.run_until_complete(FUTURE)
